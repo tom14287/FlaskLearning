acf7b3d3990770acf5a09b4f7e383fe3f611c4f5
tom14287 <534465354@qq.com>
2017-04-09 22:35:13 +0800

    update templates/user.html

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from flask import render_template
 
 app = Flask(__name__)
 
+my1list = [1, 2, 3, 4, 5, 6]
+
 @app.route('/')
 def index():
 	#response = make_response('<h1>this page needs a cookie</h1>')
@@ -15,7 +17,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name)
+	return render_template('user.html', name=name, value=my1list, comments=my1list)
 
 if __name__ == '__main__' :
 	app.debug = True

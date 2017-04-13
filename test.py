4db2a8052d8860cb0913b9432f95f03920b4b864
tom14287 <534465354@qq.com>
2017-04-13 22:50:07 +0800

    flask-bootstrap template. extend bootstrap/base.html

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import render_template
+from flask import *
+from flask_appconfig import AppConfig
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+AppConfig(app)
+Bootstrap(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+
 
 my1list = [1, 2, 3, 4, 5, 6]
 
@@ -15,9 +20,33 @@ def index():
 	#user_agent = request.headers.get('User-Agent')
 	return render_template('index.html')
 
+@app.route('/navbar')
+def navbar():
+	return render_template('navbar.html')
+
+@app.route('/navbar2')
+def navbar2():
+	return render_template('navbar2.html')
+
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name, value=my1list, comments=my1list)
+
+@app.route('/extend')
+def extend():
+	return render_template('extend.html')
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+	error = None
+
+
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
+			error = "sorry"
+		else:
+			return redirect(url_for('index'))
+	return render_template('login.html', error=error)
 
 if __name__ == '__main__' :
 	app.debug = True

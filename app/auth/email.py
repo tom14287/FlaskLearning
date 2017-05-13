810c17209634675ad805d33f26ee065321e043e8
景煌 <kiinghom@163.com>
2017-05-13 20:20:50 +0800

    写了邮箱验证和登录注册的问题

---
+++ app/auth/email.py
@@ -0,0 +1,14 @@
+from flask_mail import Message
+
+from app import mail
+from run import app
+
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_USERNAME']
+    )
+    mail.send(msg)

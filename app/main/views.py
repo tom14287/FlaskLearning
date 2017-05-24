f4fabde95e9031a62d53c4cbe71f514ff2e6c189
tom14287 <534465354@qq.com>
2017-05-24 20:01:00 +0800

    update main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 
 from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app, make_response
+    current_app, make_response, session
 
 from flask_login import login_required
 
@@ -8,12 +8,15 @@ from flask import Blueprint
 
 main_ = Blueprint('main', __name__)
 
-@main_.route("/")
+@main_.route('/')
 def index_view():
 	return render_template('index.html')
 
 
-@main_.route("/test")
-@login_required
-def link_test():
-	return render_template('about.html')
+@main_.route('/search_service', methods=['POST'])
+def search_link():
+	get_keyword = request.form['search_keyword']
+	get_type = request.form['search_type']
+
+	tag_list = []
+	return render_template('search.html', keyword=get_keyword, type=get_type, tag=tag_list)

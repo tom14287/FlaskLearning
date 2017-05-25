5318b06391bc9ff138de69c39f19c58ed12135a9
tom14287 <534465354@qq.com>
2017-05-25 22:08:40 +0800

    create test module for login/register

---
+++ test/test.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+import tempfile
+import app
+
+class FlaskTestCase(unittest.TestCase):
+	def setUp(self):
+		self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+		app.app.config['TESTING'] = True
+		self.app = app.app.test_client()
+		app.init_db()
+
+	def tearDown(self):
+		os.close(self.db_fd)
+		os.unlink(app.app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+	unittest.main()

95b7b8fa9f2c25335e0edb4703d883decb8031e6
景煌 <kiinghom@163.com>
2017-05-31 14:26:55 +0800

    designer_scheme 逻辑完成

--- app/designer/views.py
+++ app/designer/views.py
@@ -65,15 +65,15 @@ def insert_design_scheme(did, desc, img):
 	db.session.commit()
 
 #应该改成 "/scheme/<id>" 类似product
-@designer_.route("/scheme_test", methods=['GET', 'POST'])
-def designer_scheme_test():
+@designer_.route("/scheme/<id>", methods=['GET', 'POST'])
+def designer_scheme(id):
 	class Scheme():
 		def __init__(self):
 			self.id = "id_test"
 			self.description = "des_test"
 			self.designer_id = "43248329"
 
-	class Comment():
+	class Commen():
 		def __init__(self):
 			self.name = "name_test"
 			self.time = "2013._date"
@@ -84,8 +84,31 @@ def designer_scheme_test():
 			self.name = "jj"
 			self.img = "/static/img/client/1.jpg"
 	designer = De()
-	scheme_imgs = ["/static/img/product/2.jpg","/static/img/product/2.jpg","/static/img/product/2.jpg"]
+	scheme_imgs = []
 	scheme = Scheme()
-	comment = Comment()
+	comment = Commen()
 	comments = [comment]
+	designscheme = DesignScheme.query.filter_by(SchemeID=int(id)).first()
+	scheme.id = id
+	scheme.description = designscheme.SchemeDESC
+	cpath = 'app/static/img/scheme/' + id + '.jpg'
+	cpath_img = '/static/img/scheme/' + id + '.jpg'
+	with open(cpath, "wb") as f:
+		f.write(base64.b64decode(designscheme.SchemeImage))
+		f.close()
+	scheme_imgs.append(cpath_img)
+	if designscheme.DesignerID != None:
+		user = User.query.filter_by(UserID=designscheme.DesignerID).first()
+		scheme.designer_id = designscheme.DesignerID
+	else:
+		user = User.query.filter_by(UserID=designscheme.CompanyID).first()
+		scheme.designer_id = designscheme.CompanyID
+	cpath = 'app/static/img/client/' + str(user.UserID) + '.jpg'
+	cpath_img = '/static/img/client/' + str(user.UserID) + '.jpg'
+	with open(cpath, "wb") as f:
+		f.write(base64.b64decode(user.UserImage))
+		f.close()
+	designer.name = user.UserName
+	designer.img = cpath_img
 	return render_template('scheme.html', scheme=scheme, comments=comments, scheme_imgs=scheme_imgs, designer=designer)
+
